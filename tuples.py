f7f4b6770c29124e7397667d4e291975f1edfddd
LucianoRenkine <162723184+LucianoRenkine@users.noreply.github.com>
2024-05-24 12:21:59 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,18 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
 
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+    return record[1]
 
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
+# print(get_coordinate(('Scrimshawed Whale Tooth', '2A'))) == 2A
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
+    return tuple(coordinate)
 
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
-
+# print(convert_coordinate(get_coordinate(('Scrimshawed Whale Tooth', '2A')))) == ("2", "A")
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    a = convert_coordinate(get_coordinate(azara_record))
+    b = rui_record[1]
+    if a == b:
+        return azara_record + rui_record
+    else:
+        return "no coinciden"
